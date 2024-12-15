81826cb499cdf7828d2d5f8acf460630e9a309e9
Hamzakbot <shu.kurenai132010@gmail.com>
2024-12-16 00:43:25 +0530

    Update run.py

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ class WebServer():
 
 
 class RunBot():
-  room_id = "67442e976b8a17e2b34f7b0e"
+  room_id = "66ec55c386dab14260e04d14"
   bot_token = "c85b99b6b9ef44b597dc29ca2e2d452c47a3a133aafbcbb5fba7706ddd93b8e4"
   bot_file = "main"
   bot_class = "Bot"
