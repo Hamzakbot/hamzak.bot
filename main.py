f985ac84b09fb1f7774321ec3f3355a4b1425f78
Hamzakbot <shu.kurenai132010@gmail.com>
2025-03-19 06:54:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ pescar = ["ÃƒÂ°Ã…Â¸Ã‚Â¥Ã‹â€ YOU WON THE MEDAL: SILVER FISHER
 class Bot(BaseBot):
     async def on_start(self, session_metadata: SessionMetadata) -> None:
         print("working")
-        await self.highrise.walk_to(Position(14.5 , 2.0 , 6.5 , "FrontLeft"))
+        await self.highrise.walk_to(Position(0 , 0 , 0 , "FrontLeft"))
     async def on_user_join(self, user: User, position: Position | AnchorPosition) -> None:
         print(f"{user.username} entrou na sala")   
-        await self.highrise.chat(f"Welcome {user.username} to the room Ã¢ÂÂ¤Ã¯Â¸Â")
+        await self.highrise.chat(f"Welcome {user.username} to the room ❤️")
 
-        await self.highrise.chat(f" {user.username} has entered to the room Ã¢ÂÂ¤Ã¯Â¸Â")
+        await self.highrise.chat(f" {user.username} has entered to the room 💓💘")
            
         await self.highrise.send_emote("hcc-jetpack")
       
